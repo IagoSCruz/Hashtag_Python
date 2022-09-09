eb043697011773b3a41160846c05f8fb4b473867
Iago Cruz <112760040+IagoSCruz@users.noreply.github.com>
2022-09-09 14:53:25 -0300

    variaveis do programa

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
-print('Hello, World')
+print('*********************')
+print('Metas de Funcionários')
+print('*********************')
+
+#variaveis
+
+meta = 50000
+venda_func = int(input('Insira o valor de vendas: '))
+meta_loja = 90000
+vendas_loja = int(input('Insira o valor vendido pela loja: '))
+
+
+
+if venda_func >= meta and vendas_loja >= meta_loja:
+    bonus = venda_func * 0.1
+    print('O vendedor ganhou {} de bônus!'.format(bonus))
+else:
+    print('O vendedor não alcançou a meta.')
+    
