c364f76f1c5e9ba86fd4653ac537aceed44c5c13
Iago Cruz <iagosilvacontato@gmail.com>
2022-09-11 22:50:45 -0300

    condicoes elif

--- Programa_taxaInvestimento/programa.py
+++ Programa_taxaInvestimento/programa.py
@@ -1,3 +1,4 @@
+'''
 meta = 0.05
 taxa = 0
 rendimento = float(input('Insira o rendimento do fundo: '))
@@ -12,4 +13,26 @@ if rendimento > meta:
 else:
     taxa = 0
     print('A taxa foi de {}'.format(taxa))
+''' 
+# Muita repetição de código
+# Deixar o código mais limpo:
+
+
+# ACRESCIMO DO ELIF
+
+meta = 50000
+vendas_funcionario1 = int(input('Informe o total de vendas: '))
+
+if vendas_funcionario1 >= meta:
+    bonus = (0.03 * vendas_funcionario1) / 100
+    print('O funcionário obteve {} de bônus.'.format(bonus))
+
+elif vendas_funcionario1 > meta * 2:
+    bonus = (0.07 * vendas_funcionario1)
+    print('O funcionário ultrapassou a meta, obtendo {} de bônus.'.format(bonus))
+
+else:
+    bonus = 0
+    print('O funcionário não obteve bonificação.')
     
+
