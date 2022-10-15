945c8f41a39eac3422dd7888cc09e0cc10a8c2b7
Iago Cruz <iagosilvacontato@gmail.com>
2022-10-15 11:07:08 -0300

    aula sobre listas

---
+++ 07 - Indices/aula01.py
@@ -0,0 +1,8 @@
+produtos = ['tv', 'celular', 'mouse', 'teclado', 'tablet']
+vendas = [1000, 1500, 350, 270, 900]
+
+# print(vendas[1])
+vendas [3] = 300
+print ('Vendas do produto {} foram de {} unidades.'.format(produtos[3], vendas[3]))
+
+# listas são mutáveis, isso significa que podemos alterar o valor delas.
